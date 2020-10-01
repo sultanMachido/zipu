591583dd5f7249eb8b50c470a50661c41f3fde5b
John Damilola <johndamilola03@gmail.com>
2020-10-01 22:47:25 +0100

    setup project folders

---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import logger from "redux-logger";
+import reducers from "./reducers";
+import thunkMiddleware from "redux-thunk";
+
+const middleware = [thunkMiddleware, logger];
+export default function configureStore(initialState = {}) {
+  const rootReducer = combineReducers({
+    ...reducers
+  });
+  const enhancer = compose(
+    applyMiddleware(...middleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  );
+  return createStore(rootReducer, initialState, enhancer);
+}
