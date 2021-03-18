301a2e02980eb6a7fe0037d065bb1842a7a8e2e8
chiboycalix <igwechinonso77@gmail.com>
2021-03-18 23:10:14 +0100

    setup actions for transport types

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,9 +3,11 @@
 import { registerReducer } from './registration/register.reducer';
 import { verifyEmailReducer } from './verifyEmail/verifyEmail.reducer';
 import { loginReducer } from './login/login.reducer';
+import { transportReducer } from './transportType/transportType.reducer'
 
 export default {
   register: registerReducer,
   verifyEmail: verifyEmailReducer,
-  login: loginReducer
+  login: loginReducer,
+  transportTypes: transportReducer,
 };
