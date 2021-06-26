5828f6d5dbfbbff8c6bde62ef55dd0ee2a82319f
Ad3mola <ademolamadelewi@gmail.com>
2021-06-26 19:37:29 +0100

    finish setup

--- src/pages/Sample/index.jsx
+++ src/pages/Sample/index.jsx
@@ -6,7 +6,9 @@ import style from './sample.module.scss';
 let styles = classnames.bind(style);
 
 function Sample() {
-	return <div className={styles('sample-container', { 'conditional-class': true })}>Hello</div>;
+	return (
+		<div className={styles('sample-container', { 'conditional-class': true })}>How are you?</div>
+	);
 }
 
 export default Sample;
