14401753c8cf9e9f0dc34a153cfe117a9c2c757e
OlabamipeTaiwo <teeola48@gmail.com>
2021-08-12 02:32:18 -0700

    dev::header re-work

--- src/ui/components/Navigation/index.jsx
+++ src/ui/components/Navigation/index.jsx
@@ -1,6 +1,7 @@
 import classnames from 'classnames/bind';
 import { ModalContext } from 'providers/ModalProvider';
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router';
 import { Text, View } from 'ui/atoms/components/Typography';
 
 import { AuthModal } from '../AuthModal';
@@ -10,6 +11,7 @@ import style from './index.module.scss';
 const styles = classnames.bind(style);
 
 const Navigation = ({ navLinks, className }) => {
+	let history = useHistory();
 	const { showModal } = useContext(ModalContext);
 
 	const handleClick = (e, modalName) => {
@@ -23,27 +25,39 @@ const Navigation = ({ navLinks, className }) => {
 		<View className={styles('navigation-wrapper', { ['open']: className })}>
 			<View variant="ul" className={styles('nav')}>
 				{navLinks.length &&
-					navLinks.map(({ text, url, action: modalName, type, dropList, imgUrl }, index) => (
-						<li key={index}>
-							{type !== 'dropdown' && (
-								<Text
-									variant={url ? 'NavLink' : 'p'}
-									fontWeight="400"
-									to={url}
-									className={styles('links')}
-									onClick={!url && ((e) => handleClick(e, modalName))}>
-									{url && text}
+					navLinks.map(
+						({ text, url, action: modalName, type, dropList, imgUrl, isBtn, btnLink }, index) => (
+							<li key={index}>
+								{type !== 'dropdown' && (
+									<Text
+										variant={url ? 'NavLink' : 'p'}
+										fontWeight="400"
+										to={url}
+										className={styles('links')}
+										onClick={!url && ((e) => handleClick(e, modalName))}>
+										{url && text}
 
-									{modalName && (
-										<View className={styles('button-link', { [`${modalName}`]: modalName })}>
-											<View variant="button">{text}</View>
-										</View>
-									)}
-								</Text>
-							)}
-							{type === 'dropdown' && <Dropdown dropList={dropList} text={text} imgUrl={imgUrl} />}
-						</li>
-					))}
+										{isBtn && (
+											<View className={styles('button-link', { [`sign-up`]: 'sign-up' })}>
+												<View onClick={() => history.push(btnLink)} variant="button">
+													{text}
+												</View>
+											</View>
+										)}
+
+										{modalName && (
+											<View className={styles('button-link', { [`${modalName}`]: modalName })}>
+												<View variant="button">{text}</View>
+											</View>
+										)}
+									</Text>
+								)}
+								{type === 'dropdown' && (
+									<Dropdown dropList={dropList} text={text} imgUrl={imgUrl} />
+								)}
+							</li>
+						)
+					)}
 			</View>
 			<AuthModal signUpProps="signup" loginProps="login" />
 		</View>
---
+++ src/utils/PublicRoute.js
@@ -0,0 +1,31 @@
+/* eslint-disable react/display-name */
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect, Route } from 'react-router-dom';
+
+const mapStateToProps = (state) => ({
+	login: state.login
+});
+
+const PublicRoute = ({ component: Component, login: { token, isAuthenticated }, ...rest }) => {
+	let isLoggedIn = false;
+	if (token && isAuthenticated) {
+		isLoggedIn = true;
+	}
+
+	return (
+		<Route
+			{...rest}
+			render={(props) =>
+				isLoggedIn ? (
+					//   <Redirect to={roleToRoute(user_roles[0].name)} />
+					<Redirect to="/customer/booking-history" />
+				) : (
+					<Component {...props} />
+				)
+			}
+		/>
+	);
+};
+
+export default connect(mapStateToProps, {})(PublicRoute);
